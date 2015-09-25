04500aa1430fa5e15832626eea70972070e8de62
Jacob Parker <jacobparker1992@gmail.com>
2015-09-25 22:13:52 +0100

    More math!

--- math/entity.js
+++ math/entity.js
@@ -12,116 +12,118 @@ const toZeroEntity = always({ ...entity, value: 0 });
 
 const sign = nthArg(0);
 const context = nthArg(1);
-const a = nthArg(2);
-const aIsZero = pipe(a, isZero);
-const aValueNil = pipe(a, valueNil);
-const aUnits = pipe(a, prop('units'));
-const aUnitKeys = pipe(aUnits, keys);
-const aUnitKeysLength = pipe(aUnitKeys, length);
-const aSymbols = pipe(a, prop('symbols'));
-const aBaseDimensions = converge(baseDimensions, context, a);
-const aToSi = converge(toSi, context, a);
 
-const b = nthArg(3);
-const rhsIsZero = pipe(b, isZero);
-const rhsValueNil = pipe(b, valueNil);
-const bUnits = pipe(b, prop('units'));
-const bUnitKeys = pipe(bUnits, keys);
-const bUnitKeysLength = pipe(bUnitKeys, length);
-const bSymbols = pipe(b, prop('symbols'));
-const bBaseDimensions = converge(baseDimensions, context, b);
-const bToSi = converge(toSi, context, b);
-const bHasUnits = pipe(bUnitKeys, isEmpty);
-const bHasSymbols = pipe(bSymbols, keys, length);
-const bNotPureNumericEntity = complement(either(bHasUnits, bHasSymbols));
+const lhs = nthArg(2);
+const lhsIsZero = pipe(lhs, isZero);
+const lhsValueNil = pipe(lhs, valueNil);
+const lhsUnits = pipe(lhs, prop('units'));
+const lhsUnitKeys = pipe(lhsUnits, keys);
+const lhsUnitKeysLength = pipe(lhsUnitKeys, length);
+const lhsSymbols = pipe(lhs, prop('symbols'));
+const lhsBaseDimensions = converge(baseDimensions, context, lhs);
+const lhsToSi = converge(toSi, context, lhs);
 
-const eitherIsZero = either(aIsZero, rhsIsZero);
-const eitherValueNil = either(aValueNil, rhsValueNil);
-const symbolsDiffer = converge(pipe(equals, not), aSymbols, bSymbols);
-const unitsMatch = converge(equals, aUnits, bUnits);
-const baseDimensionsMatch = converge(equals, aBaseDimensions, bBaseDimensions);
+const rhs = nthArg(3);
+const rhsIsZero = pipe(rhs, isZero);
+const rhsValueNil = pipe(rhs, valueNil);
+const rhsUnits = pipe(rhs, prop('units'));
+const rhsUnitKeys = pipe(rhsUnits, keys);
+const rhsUnitKeysLength = pipe(rhsUnitKeys, length);
+const rhsSymbols = pipe(rhs, prop('symbols'));
+const rhsBaseDimensions = converge(baseDimensions, context, rhs);
+const rhsToSi = converge(toSi, context, rhs);
+const rhsHasUnits = pipe(rhsUnitKeys, isEmpty);
+const rhsHasSymbols = pipe(rhsSymbols, keys, length);
+const rhsNotPureNumericEntity = complement(either(rhsHasUnits, rhsHasSymbols));
+
+const eitherIsZero = either(lhsIsZero, rhsIsZero);
+const eitherValueNil = either(lhsValueNil, rhsValueNil);
+
+const symbolsMatch = converge(equals, lhsSymbols, rhsSymbols);
+const symbolsDiffer = complement(symbolsMatch);
+const unitsMatch = converge(equals, lhsUnits, rhsUnits);
+const baseDimensionsMatch = converge(equals, lhsBaseDimensions, rhsBaseDimensions);
 
 
-const bySign = (direction, v1) => ({ ...v1, value: v1.value * direction });
-const performAddMath = (direction, v1, v2) => ({ ...v1, value: v1.value + v2.value * direction });
+const flipRhsBySign = (direction, ctx, left, right) => ({ ...right, value: right.value * direction });
+const performAddMath = (direction, ctx, left, right) => ({ ...left, value: left.value + right.value * direction });
 // (sign: (1, -1), context: Context, lhs: Entity, rhs: Entity) => Entity
 const abstractMathAdd = cond([
   [eitherValueNil, toNil],
-  [rhsIsZero, a],
-  [aIsZero, converge(bySign, sign, b)],
+  [rhsIsZero, lhs],
+  [lhsIsZero, flipRhsBySign],
   [symbolsDiffer, toNil],
-  [unitsMatch, converge(performAddMath, sign, a, b)],
-  [baseDimensionsMatch, converge(performAddMath, sign, aToSi, bToSi)],
+  [unitsMatch, performAddMath],
+  [baseDimensionsMatch, converge(performAddMath, sign, context, lhsToSi, rhsToSi)],
   [T, toNil],
 ]);
 
 
-const overlapLength = pipe(
-  converge(intersection, bUnitKeys, aUnitKeys),
+const lhsRhsUnitsOverlapLength = pipe(
+  converge(intersection, rhsUnitKeys, lhsUnitKeys),
   length
 );
-const lhsAndRhsKeysLengthEqualsOverlapKeysLength = both(
-  equals(nthArg(0), nthArg(1)),
-  equals(nthArg(0), nthArg(2))
+const allArgsEqual = pipe(
+  unapply(identity), // Args to array
+  uniq,
+  length,
+  equals(1),
 );
-const needConversion = converge(ifElse(
-    lhsAndRhsKeysLengthEqualsOverlapKeysLength, T, F
-), overlapLength, aUnitKeysLength, bUnitKeysLength);
+const lhsKeysLengthEqRhsKeysLengthEqOverlapKeysLength = converge(allArgsEqual,
+  lhsRhsUnitsOverlapLength, lhsUnitKeysLength, rhsUnitKeysLength);
+const needConversion = complement(lhsKeysLengthEqRhsKeysLengthEqOverlapKeysLength);
 
-const lhsUnitPairs = pipe(nthArg(0), toPairs);
-const lhsUnits = pipe(nthArg(0), prop('units'));
-const lhsSymbols = pipe(nthArg(0), prop('symbols'));
-const rhsUnitPairs = pipe(nthArg(1), toPairs);
-const rhsUnits = pipe(nthArg(1), prop('units'));
-const rhsSymbols = pipe(nthArg(1), prop('symbols'));
+const mergeMultiplicationLhsUnitPairs = pipe(nthArg(0), toPairs);
+const mergeMultiplicationRhsUnitPairs = pipe(nthArg(1), toPairs);
 
 const mergeMultiplicationUnitSymbols = pipe(
-  converge(concat, lhsUnitPairs, rhsUnitPairs),
+  converge(concat, mergeMultiplicationLhsUnitPairs, mergeMultiplicationRhsUnitPairs),
   groupBy(head),
   mapObj(sumLastElementsInPairs),
   pickBy(notNil),
 );
 
-const performMultiplyMath = (direction, lhs, rhs) => ({
+const performMultiplyMath = (direction, ctx, left, right) => ({
   ...entity,
-  value: lhs.value * (rhs.value ** direction),
-  units: converge(mergeMultiplicationUnitSymbols, lhsUnits, rhsUnits),
-  symbols: converge(mergeMultiplicationUnitSymbols, lhsSymbols, rhsSymbols),
+  value: left.value * (right.value ** direction),
+  units: mergeMultiplicationUnitSymbols(left.units, right.units),
+  symbols: mergeMultiplicationUnitSymbols(left.symbols, right.symbols),
 }); // FIXME 'All unitless properties shouldn't carry after multiplication'
 
 const abstractMathMultply = cond([
   [eitherValueNil, toNil],
   [eitherIsZero, toZeroEntity],
   [T, ifElse(needConversion,
-    converge(performMultiplyMath, sign, aToSi, bToSi),
-    converge(performMultiplyMath, sign, a, b),
+    converge(performMultiplyMath, sign, context, lhsToSi, rhsToSi),
+    performMultiplyMath,
   )],
 ]);
 
 
-const performExponentMath = (direction, lhs, rhs) => ({
+const performExponentMath = (direction, ctx, left, right) => ({
   ...entity,
-  value: lhs.value ** (rhs.value * direction),
-  units: mapObj(multiply(rhs.value), lhs.units),
-  symbols: mapObj(multiply(rhs.value), lhs.symbols),
+  value: left.value ** (right.value * direction),
+  units: mapObj(multiply(right.value), left.units),
+  symbols: mapObj(multiply(right.value), left.symbols),
 });
 
 const abstractMathExponent = cond([
   [eitherValueNil, toNil],
-  [bNotPureNumericEntity, toNil],
-  [T, converge(performExponentMath, sign, a, b)],
+  [rhsNotPureNumericEntity, toNil],
+  [T, performExponentMath],
 ]);
 
 const valueAdd = partial(abstractMathAdd, 1);
 const valueSubtract = partial(abstractMathAdd, -1);
 const valueMultiply = partial(abstractMathMultply, 1);
 const valueDivide = partial(abstractMathMultply, -1);
-const valueExporent = partial(abstractMathExponent, 1);
+const valueExponent = partial(abstractMathExponent, 1);
+// No inverse exponent
 
 export {
   valueAdd as add,
   valueSubtract as subtract,
   valueMultiply as multiply,
   valueDivide as divide,
-  valueExporent as exporent,
+  valueExponent as exponent,
 };
