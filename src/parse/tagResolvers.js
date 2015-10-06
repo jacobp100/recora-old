9188dd58ab12be2c0b5c592317f1aeeb14b448f7
Jacob Parker <jacobparker1992@gmail.com>
2015-10-06 21:38:39 +0100

    Stuff

--- src/parse/tagResolvers.js
+++ src/parse/tagResolvers.js
@@ -1,6 +1,28 @@
 import { entity } from '../types';
 import assert from 'assert';
 
+
+function tagUnitSymbol(unitOrSymbol) {
+  return function tagUnitOrSymbolFn(values, { value, power }) {
+    const lastItem = last(values);
+
+    if (lastItem.type === entity.type) {
+      return adjust(evolve({
+        [unitOrSymbol]: over(
+          lensProp(value),
+          pipe(
+            defaultTo(0),
+            add(power),
+          ),
+        ),
+      }), -1, values);
+    }
+
+    return append(assocPath([unitOrSymbol, value], power, entity), values);
+  };
+}
+
+
 export function TAG_NUMBER(values, { value }) {
   assert(typeof value === 'number');
   const lastItem = last(values);
@@ -10,44 +32,6 @@ export function TAG_NUMBER(values, { value }) {
   }
 
   return append({ ...entity, value }, values);
-}
-
-export function TAG_UNIT(values, { value, power }) {
-  // This code is almost identical for symbols (s/unit/symbol/g)
-  const lastItem = last(values);
-
-  if (lastItem.type === entity.type) {
-    return adjust(evolve({
-      units: over(
-        lensProp(value),
-        pipe(
-          defaultTo(0),
-          add(power),
-        ),
-      ),
-    }), -1, values);
-  }
-
-  return append(assocPath(['units', value], power, entity), values);
-}
-
-export function TAG_SYMBOL(values, { value, power }) {
-  // This code is almost identical for symbols (s/unit/symbol/g)
-  const lastItem = last(values);
-
-  if (lastItem.type === entity.type) {
-    return adjust(evolve({
-      symbols: over(
-        lensProp(value),
-        pipe(
-          defaultTo(0),
-          add(power),
-        ),
-      ),
-    }), -1, values);
-  }
-
-  return append(assocPath(['symbols', value], power, entity), values);
 }
 
 export const TAG_NOOP = append(entity);
